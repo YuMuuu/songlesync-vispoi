eb4aa615916be3ead2f5eff529dbc12db2d85a8a
YuMuuu <c01150788f@edu.teu.ac.jp>
2018-10-29 16:06:22 +0900

    update fs
    ledを使えるようにした

--- slave/index.js
+++ slave/index.js
@@ -2,14 +2,23 @@
 exports.__esModule = true;
 var sa = require("songle-api");
 var sw = require("songle-widget");
+var fs = require("fs");
 var settings = require("./settings");
 var player = new sa.Player({
     accessToken: settings.tokens.access
 });
+var dir = "/sys/class/gpio";
+var gpio2 = dir + "/gpio2";
+var count = 0;
+fs.writeFileSync(dir + '/export', 2); // 2ピン
+fs.writeFileSync(gpio2 + '/direction', 'out'); // 出力に設定
+var flash = function () {
+    fs.writeFileSync(gpio2 + '/value', 0.1);
+    setTimeout(flash, 500);
+    fs.writeFileSync(dir + '/unexport', 2);
+};
 player.addPlugin(new sa.Plugin.Beat());
 player.addPlugin(new sw.Plugin.Chord());
-//player.addPlugin(new sw.Plugin.Melody());
-//player.addPlugin(new sw.Plugin.Chorus());
 player.addPlugin(new sa.Plugin.SongleSync());
 player.on("play", function (ev) { return console.log("play"); });
 player.on("seek", function (ev) { return console.log("seek"); });
@@ -17,16 +26,9 @@ player.on("pause", function (ev) { return console.log("pause"); });
 player.on("finish", function (ev) { return console.log("finish"); });
 player.on("beatPlay", function (ev) {
     console.log("beat:", ev.data.beat.position);
-    //console.log(ev)
+    flash;
 });
 player.on("chordPlay", function (ev) {
-    //console.log("chordPlay")
-    // console.log("duration:", ev.data.chord.duration)
     console.log("chordName:", ev.data.chord.name);
-    //console.log(ev)
 });
-// player.on("chorusEnter", (ev) => console.log("chorusEnter"));
-// player.on("chorusLeave", (ev) => console.log("chorusLeave"));
-// player.on("repeatEnter", (ev) => console.log("repeatEnter"));
-// player.on("repeatLeave", (ev) => console.log("repeatLeave"));
 setInterval(function () { }, 10000);
--- slave/index.ts
+++ slave/index.ts
@@ -1,6 +1,6 @@
-import sa = require("songle-api");　
-import sw = require("songle-widget"); 
-import fs = require('fs'); 
+import sa = require("songle-api");
+import sw = require("songle-widget");
+import fs = require('fs');
 import settings = require("./settings");
 
 
@@ -8,32 +8,39 @@ const player = new sa.Player({
     accessToken: settings.tokens.access
 });
 
+const dir: String = "/sys/class/gpio";
+const gpio2: String = dir + "/gpio2";
+
+
+let count = 0;
+
+
+fs.writeFileSync(dir + '/export', 2); // 2ピン
+fs.writeFileSync(gpio2 + '/direction', 'out'); // 出力に設定
+
+
+let flash = (): void  => {
+    fs.writeFileSync(gpio2 + '/value', 0.1);
+    setTimeout(flash, 500);
+    fs.writeFileSync(dir + '/unexport', 2); 
+};
 
 player.addPlugin(new sa.Plugin.Beat());
 player.addPlugin(new sw.Plugin.Chord());
-//player.addPlugin(new sw.Plugin.Melody());
-//player.addPlugin(new sw.Plugin.Chorus());
 player.addPlugin(new sa.Plugin.SongleSync());
 
 
-player.on("play",        (ev) => console.log("play"));
-player.on("seek",        (ev) => console.log("seek"));
-player.on("pause",       (ev) => console.log("pause"));
-player.on("finish",      (ev) => console.log("finish"));
-player.on("beatPlay",    (ev) => {
+player.on("play", (ev) => console.log("play"));
+player.on("seek", (ev) => console.log("seek"));
+player.on("pause", (ev) => console.log("pause"));
+player.on("finish", (ev) => console.log("finish"));
+player.on("beatPlay", (ev) => {
     console.log("beat:", ev.data.beat.position)
-    //console.log(ev)
+    flash
 });
-player.on("chordPlay",   (ev) => {
-    //console.log("chordPlay")
-    // console.log("duration:", ev.data.chord.duration)
+player.on("chordPlay", (ev) => {
     console.log("chordName:", ev.data.chord.name)
-    //console.log(ev)
 });
-// player.on("chorusEnter", (ev) => console.log("chorusEnter"));
-// player.on("chorusLeave", (ev) => console.log("chorusLeave"));
-// player.on("repeatEnter", (ev) => console.log("repeatEnter"));
-// player.on("repeatLeave", (ev) => console.log("repeatLeave"));
 
 
 setInterval(() => { }, 10000);
