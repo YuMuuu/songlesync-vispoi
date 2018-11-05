1e0343bcfd14f2547e157fd6afc32511c40719c8
YuMuuu <c01150788f@edu.teu.ac.jp>
2018-11-05 11:12:58 +0000

    add slave wx281xw

--- slave/index.ts
+++ slave/index.ts
@@ -3,27 +3,12 @@ import sw = require("songle-widget");
 import fs = require('fs');
 import settings = require("./settings");
 
+import ws281x = require("../lib/ws281x-native");
 
 const player = new sa.Player({
     accessToken: settings.tokens.access
 });
 
-const dir: String = "/sys/class/gpio";
-const gpio2: String = dir + "/gpio2";
-
-
-let count = 0;
-
-
-fs.writeFileSync(dir + '/export', 2); // 2ピン
-fs.writeFileSync(gpio2 + '/direction', 'out'); // 出力に設定
-
-
-let flash = (): void  => {
-    fs.writeFileSync(gpio2 + '/value', 0.1);
-    setTimeout(flash, 500);
-    fs.writeFileSync(dir + '/unexport', 2); 
-};
 
 player.addPlugin(new sa.Plugin.Beat());
 player.addPlugin(new sw.Plugin.Chord());
@@ -36,7 +21,6 @@ player.on("pause", (ev) => console.log("pause"));
 player.on("finish", (ev) => console.log("finish"));
 player.on("beatPlay", (ev) => {
     console.log("beat:", ev.data.beat.position)
-    flash
 });
 player.on("chordPlay", (ev) => {
     console.log("chordName:", ev.data.chord.name)
